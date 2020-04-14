314b3dd5f1982563e161cc17574419dba191679d
Raoul Kent <raoulkent@gmail.com>
2020-04-14 11:58:49 +0200

    Refactor onClick of btn_newgame to run newGame()

--- development/SimpleGolf/app/src/main/java/com/example/simplegolf/MainActivity.java
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simplegolf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,19 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setListeners();
     }
 
-    private void setListeners() {
-
-        Button newGame = (Button) findViewById(R.id.btn_newgame);
-        newGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO: Functionality to start a 'new game activity'
-            }
-        });
-
-        // TODO: Add more buttons for multiplayer etc.
+    private void newGame(View view){
+        // TODO: Create new activity and add to below intent
+//        Intent intent = new Intent(this, GameActivity.class);
+//        startActivity(intent);
     }
 }
