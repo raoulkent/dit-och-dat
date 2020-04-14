f470e7c8f49b73df401dbe554aac3cfbfea23c80
Raoul Kent <raoulkent@gmail.com>
2020-04-14 11:48:33 +0200

    Add splash screen with new game button

--- development/SimpleGolf/app/src/main/java/com/example/simplegolf/MainActivity.java
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.simplegolf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListeners();
+    }
+
+    private void setListeners() {
+
+        Button newGame = (Button) findViewById(R.id.btn_newgame);
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Functionality to start a 'new game activity'
+            }
+        });
+
+        // TODO: Add more buttons for multiplayer etc.
     }
 }
