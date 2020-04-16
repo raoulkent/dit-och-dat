5cc817a27bcd85c0c7c181ca3b4248cb6837356d
xslimShadyx <husseinbja@yahoo.de>
2020-04-16 14:27:15 +0200

    Update Player.java

--- development/SimpleGolf/app/src/main/java/com/example/simplegolf/model/Player.java
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/model/Player.java
@@ -7,32 +7,18 @@ import java.util.List;
  * This class is responsible of making a player
  */
  class Player {
-   private String firstName ;
-   private String lastName;
-   private List<Integer> shots = new ArrayList<>();
+    private String initials;
+    private  int [] shots ;
 
-    public Player(String firstName,String lastName ){
-
-        this.firstName =firstName;
-        this.lastName=lastName;
+    public Player(String initials,int a ){
+        this.initials=initials;
+        this.shots=new int[a];
     }
 
-    public String getFirstNameName(){
-        return firstName;
-    }
-    public String getLastNameName(){
-        return lastName;
+
+    public int getShotsForHole(int i){
+        return  shots[i];
     }
 
-    public List<Integer>getShots(){
-        return shots;
-    }
 
-    /**
-     * This method give you the 3 first letters form the first name as Initials
-     * @return A string Of 3 Letters .
-     */
-    public String getInitials(){
-        return firstName.substring(0,3);
-    }
 }
