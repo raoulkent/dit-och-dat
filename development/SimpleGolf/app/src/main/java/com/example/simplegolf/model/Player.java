dbac62018ea26a8c0a98065b561061c6ce1e5b4f
xslimShadyx <husseinbja@yahoo.de>
2020-04-16 13:06:06 +0200

    Update Player.java
    Add the new Variables and methods

--- development/SimpleGolf/app/src/main/java/com/example/simplegolf/model/Player.java
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/model/Player.java
@@ -1,16 +1,38 @@
 package com.example.simplegolf.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class is responsible of making a player
  */
  class Player {
-   private String name ;
+   private String firstName ;
+   private String lastName;
+   private List<Integer> shots = new ArrayList<>();
 
-    public Player(String name){
-        this.name = name;
+    public Player(String firstName,String lastName ){
+
+        this.firstName =firstName;
+        this.lastName=lastName;
     }
 
-    public String getName(){
-        return name;
+    public String getFirstNameName(){
+        return firstName;
+    }
+    public String getLastNameName(){
+        return lastName;
+    }
+
+    public List<Integer>getShots(){
+        return shots;
+    }
+
+    /**
+     * This method give you the 3 first letters form the first name as Initials
+     * @return A string Of 3 Letters .
+     */
+    public String getInitials(){
+        return firstName.substring(0,3);
     }
 }
