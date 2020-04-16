3664d21efe88f2f8f0e260f127643f4a1e96f552
Raoul Kent <raoulkent@gmail.com>
2020-04-16 14:35:49 +0200

    Modify Player to have complete getters and setters

--- development/SimpleGolf/app/src/main/java/com/example/simplegolf/model/Hole.java
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/model/Hole.java
@@ -3,27 +3,29 @@ package com.example.simplegolf.model;
 /**
  * This class is responsible of holding the information about a hole
  */
- class Hole {
-     private int hcp ;
-     private int par;
+class Hole {
+    private int hcp;
+    private int par;
 
-    public Hole(){
-        this.hcp=0;
-        this.par=0;
+    Hole() {
+        this.hcp = 0;
+        this.par = 0;
     }
 
-    public int getHcp(){
+    public int getHcp() {
         return hcp;
     }
-    public int getPar(){
+
+    public int getPar() {
         return par;
     }
 
-    public void setPar(int a){
-        par=a;
+    public void setPar(int a) {
+        par = a;
     }
-    public void setHcp(int a){
-        hcp=a;
+
+    public void setHcp(int a) {
+        hcp = a;
     }
 
 }
--- development/SimpleGolf/app/src/main/java/com/example/simplegolf/model/Player.java
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/model/Player.java
@@ -6,19 +6,29 @@ import java.util.List;
 /**
  * This class is responsible of making a player
  */
- class Player {
+class Player {
     private String initials;
-    private  int [] shots ;
+    private int[] shots;
 
-    public Player(String initials,int a ){
-        this.initials=initials;
-        this.shots=new int[a];
+    public Player(String initials, int a) {
+        this.initials = initials;
+        this.shots = new int[a];
     }
 
-
-    public int getShotsForHole(int i){
-        return  shots[i];
+    public String getInitials() {
+        return initials;
     }
 
+//  TODO: Enable this if there's a need to change initials
+//    public void setInitials(String initials) {
+//        this.initials = initials;
+//    }
 
+    public void setShotsShotsForHole(int holeNumber, int shots) {
+        this.shots[holeNumber] = shots;
+    }
+
+    public int getShotsForHole(int holeNumber) {
+        return shots[holeNumber];
+    }
 }
