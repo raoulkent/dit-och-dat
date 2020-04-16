89794b2d31b1173ce401ec13cf56bbf88e3b1881
JoarHellqvist <62557759+JoarHellqvist@users.noreply.github.com>
2020-04-16 15:43:12 +0200

    Added NumberPicker to GameActivity and activity_game

--- development/SimpleGolf/app/src/main/java/com/example/simplegolf/GameActivity.java
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/GameActivity.java
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.example.simplegolf.model.Scorecard;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
 
     private Scorecard scorecard;
 
@@ -18,13 +19,24 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        NumberPicker nrHolesPicker = findViewById(R.id.nrHolesPicker);
+        nrHolesPicker.setMinValue(1);
+        nrHolesPicker.setMaxValue(18);
+        nrHolesPicker.setValue(9);
+
+
+        nrHolesPicker.setOnValueChangedListener(this);
+
         //TODO: Add functionality to button
         // method to generate the scorecard
     }
 
     public void  onClickCreate(View view){
-        TextView nrOfHoles = findViewById(R.id.nrOfHoleSelect);
-        int nrHoles = Integer.parseInt(nrOfHoles.getText().toString());
+
+        NumberPicker nrHolesPicker = findViewById(R.id.nrHolesPicker);
+        int nrHoles = nrHolesPicker.getValue();
+        //TextView nrOfHoles = findViewById(R.id.nrOfHoleSelect);
+        //int nrHoles = Integer.parseInt(nrOfHoles.getText().toString());
         scorecard = new Scorecard(nrHoles);
 
         //Send holes to GameOverview for now, this will change to game object
@@ -34,4 +46,8 @@ public class GameActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+
+    }
 }
