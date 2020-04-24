b9c8c2e8d608ac7524702b58690c1bbe3feb1713
Philip Tibom <philiptibom@gmail.com>
2020-04-24 22:21:53 +0200

    Fixed HoleTest.java after refactor

--- development/SimpleGolf/app/src/test/java/com/example/simplegolf/model/HoleTest.java
+++ development/SimpleGolf/app/src/test/java/com/example/simplegolf/model/HoleTest.java
@@ -6,35 +6,17 @@ import static org.junit.Assert.assertEquals;
 
 public class HoleTest {
 
-
-    @Test
-    public void testGetHcp(){
-       Hole hole = new Hole();
-       int expected = 0;
-       int actual = hole.getHcp();
-       assertEquals(actual,expected);
-    }
-
-    @Test
-    public void testSetHcp(){
-        Hole hole = new Hole();
-        int expected = 2;
-        hole.setHcp(expected);
-        int actual = hole.getHcp();
-        assertEquals(actual,expected);
-    }
-
     @Test
     public void testGetPar(){
-        Hole hole = new Hole();
-        int expected = 0;
+        Hole hole = new Hole(3, 5);
+        int expected = 5;
         int actual = hole.getPar();
         assertEquals(actual,expected);
     }
 
     @Test
     public void testSetPar(){
-        Hole hole = new Hole();
+        Hole hole = new Hole(3, 5);
         int expected = 5;
         hole.setPar(expected);
         int actual = hole.getPar();
