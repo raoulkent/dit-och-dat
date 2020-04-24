31c9895ca4af67141e3188da31e521d299745bd2
xslimShadyx <husseinbja@yahoo.de>
2020-04-24 11:16:13 +0200

    code style  fixed

--- development/SimpleGolf/app/src/test/java/com/example/simplegolf/model/PlayerTest.java
+++ development/SimpleGolf/app/src/test/java/com/example/simplegolf/model/PlayerTest.java
@@ -8,73 +8,48 @@ public class PlayerTest {
 
     @Test
     public void testInitials(){
-        //Arrange
         String expected = "HUS";
         Player player = new Player(expected ,0);
-
-        //Act
         String actual= player.getInitials();
-
-        //Test
         assertEquals(actual,expected);
     }
 
     @Test
     public void testSetShotsShotsForHole(){
-        //Arrange
         int expected = 10;
         Player player = new Player("HUS",2);
-
-        //Act
         player.setShotsShotsForHole(0,10);
         int actual = player.getShotsForHole(0);
-
-        //Test
         assertEquals(actual,expected);
     }
 
     @Test
     public void testGetTotalShots(){
-        //Arrange
         int expected = 20;
         Player player = new Player("HUS",2);
-
-        //Act
         player.setShotsShotsForHole(0,10);
         player.setShotsShotsForHole(1,10);
         int actual = player.getTotalShots();
-
-        //Test
         assertEquals(actual,expected);
     }
 
     @Test
     public void testIncrementHole(){
-        //Arrange
         Player player = new Player("HUS",2);
         int expected = 3;
-
-        //Act
         player.setShotsShotsForHole(0,2);
         player.incrementHole(0);
         int actual = player.getShotsForHole(0);
-
-        //Test
         assertEquals(actual,expected);
     }
 
     @Test
     public void testDecrementHole(){
-        //Arrange
         Player player = new Player("HUS",2);
         int expected = 1;
-
-        //Act
         player.setShotsShotsForHole(0,2);
         player.decrementHole(0);
         int actual = player.getShotsForHole(0);
-
-        //Test
         assertEquals(actual,expected);
     }
 }
--- development/SimpleGolf/app/src/test/java/com/example/simplegolf/model/ScorecardTest.java
+++ development/SimpleGolf/app/src/test/java/com/example/simplegolf/model/ScorecardTest.java
@@ -9,55 +9,35 @@ public class ScorecardTest {
 
     @Test
     public void testAddPlayer(){
-        //Arrange
         Scorecard scorecard = new Scorecard(5);
         String expected = "Hus";
-
-        //Act
         scorecard.addPlayer(expected);
         String actual = scorecard.getPlayers().get(0).getInitials();
-
-        //Test
         assertEquals(actual,expected);
 
     }
     @Test
     public void testGetHoles(){
-        //Arrange
         int expected = 5;
         Scorecard scorecard = new Scorecard(expected);
-
-        //Act
         int actual = scorecard.getHoles().size();
-
-        //Test
         assertEquals(actual,expected);
 
     }
 
     @Test
     public void testGetNumberOfHoles(){
-        //Arrange
         int expected = 5;
         Scorecard scorecard = new Scorecard(expected);
-
-        //Act
         int actual = scorecard.getNumberOfHoles();
-
-        //Test
         assertEquals(actual,expected);
     }
 
     @Test
     public void testGetPlayers(){
-        //Arrange
         int expected = 0;
         Scorecard scorecard = new Scorecard(5);
-
-        //Act
         int actual = scorecard.getPlayers().size();
-
-        //Test
         assertEquals(actual,expected);
     }
 }
