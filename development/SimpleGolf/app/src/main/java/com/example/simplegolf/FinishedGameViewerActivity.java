7b7e1f28c69a5e42e4d711340646d274a36cee96
Alfred Kjeller <alfred.kjeller@telia.com>
2020-05-21 14:36:26 +0200

    Finished gameviewer done

---
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/FinishedGameViewerActivity.java
@@ -0,0 +1,39 @@
+package com.example.simplegolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.simplegolf.ui.scorecard.ScorecardFragment;
+
+public class FinishedGameViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finished_game_viewer_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, new ScorecardFragment())
+                    .commitNow();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_name_main:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
