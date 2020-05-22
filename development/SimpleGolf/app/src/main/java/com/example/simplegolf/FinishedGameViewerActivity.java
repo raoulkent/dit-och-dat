76b5930ff870afab9254cb8fe8a1195fea973923
Alfred Kjeller <alfred.kjeller@telia.com>
2020-05-22 09:24:28 +0200

    Removed back button from old games

--- development/SimpleGolf/app/src/main/java/com/example/simplegolf/FinishedGameViewerActivity.java
+++ development/SimpleGolf/app/src/main/java/com/example/simplegolf/FinishedGameViewerActivity.java
@@ -3,8 +3,6 @@ package com.example.simplegolf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.simplegolf.ui.scorecard.ScorecardFragment;
 
@@ -19,21 +17,5 @@ public class FinishedGameViewerActivity extends AppCompatActivity {
                     .replace(R.id.container, new ScorecardFragment())
                     .commitNow();
         }
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mainmenu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.action_name_main:
-                finish();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
