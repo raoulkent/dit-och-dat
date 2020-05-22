70b2244e5bfa31a17cd5c6acc65cea01fe86db21
Philip Tibom <philiptibom@gmail.com>
2020-05-22 22:59:19 +0200

    Basic rest server

--- development/backend/src/main/java/Server.java
+++ development/backend/src/main/java/Server.java
@@ -1,5 +1,12 @@
+import com.google.gson.Gson;
+
+import static spark.Spark.*;
+
 public class Server {
     public static void main(String[] args) {
-        
+        Gson gson = new Gson();
+        get("/hello", "application/json", (req, res) -> {
+            return "Hello world";
+        }, gson::toJson);
     }
 }
